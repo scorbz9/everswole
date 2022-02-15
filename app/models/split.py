cead37694fc4f275ea76bbd89d94746406ba8348
Seth Corbett <scorbz9@gmail.com>
2022-02-15 10:32:03 -0800

    configured database with split/day/exercise/user models

--- app/models/split.py
+++ app/models/split.py
@@ -8,6 +8,5 @@ class Split(db.Model):
     name = db.Column(db.String(50), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     start_date = db.Column(db.DateTime(timezone=True), server_default=func.now())
-    end_date = db.DateTime()
 
     user = db.relationship("User", back_populates="splits")
