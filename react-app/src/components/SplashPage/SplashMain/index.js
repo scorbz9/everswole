266ef4703beab77760d84bc8343b906dac8ec10e
Seth Corbett <scorbz9@gmail.com>
2022-02-15 14:44:44 -0800

    changed main color to red and began styling splash main component

--- react-app/src/components/SplashPage/SplashMain/index.js
+++ react-app/src/components/SplashPage/SplashMain/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-
+import './SplashMain.css'
 
 
 const SplashMain = () => {
     return (
         <div className="splash-main-container">
             <h1 className="greeting">
-                Tame your workout, organize your life
+                Tame your workout, organize your gains
             </h1>
             <h2 className="greeting-subtitle">
-                Remember everything and tackle any workout with your exercises, notes, and weekly schedule all in one place."
+                Remember everything and tackle any workout with your exercises, notes, and weekly schedule all in one place
             </h2>
             <button className='splash-main-sign-up'>Sign up for free</button>
             <a href="/login">Already have an account? Log in</a>
