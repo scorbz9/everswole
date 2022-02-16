d810cb7f01c2a3d92d0c65bcfe7342cd3597dfef
Seth Corbett <scorbz9@gmail.com>
2022-02-16 13:05:34 -0800

    modified scroll bar styling

--- react-app/src/components/SplitDashboard/SplitDashboardHero/index.js
+++ react-app/src/components/SplitDashboard/SplitDashboardHero/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import plates4 from '../../img/everswole_reverse.jpg'
+import plates from '../../img/everswole_reverse.jpg'
 
 import './SplitDashboardHero.css'
 
@@ -9,7 +9,7 @@ const SplitDashboardHero = () => {
 
     return (
         <div className="split-dashboard-hero-container">
-            <img src={plates4} className="split-dashboard-hero-image"></img>
+            <img src={plates} className="split-dashboard-hero-image"></img>
             <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
