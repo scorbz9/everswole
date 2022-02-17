5abe2ef6904cb42a50f0d57267971a86d2cfb0ab
Seth Corbett <scorbz9@gmail.com>
2022-02-17 11:25:23 -0800

    created form inputs + programmatically read-in inputs from array state variable, which will be updated to allow for a custom amount of inputs

--- react-app/src/components/SplitDashboard/SplitDashboardMain/AddDayForm/index.js
+++ react-app/src/components/SplitDashboard/SplitDashboardMain/AddDayForm/index.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React , { useState } from "react";
+import { useDispatch } from "react-redux";
 
 import './AddDayForm.css'
 
 const AddDayForm = () => {
+    const dispatch = useDispatch();
+
+    const [name, setName] = useState("");
+    const [workoutInputList, setWorkoutInputList] = useState([""])
+
+    const updateWorkoutInputList = (e, index) => {
+        const name = e.target.value;
+        const list = [...workoutInputList]
+        list[index] = name
+        setWorkoutInputList(list);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        const payload = {
+
+        }
+
+        console.log(payload)
+
+        // temp thunk action until its written
+        // await dispatch(temp(payload))
+    }
 
     return (
         <div className="add-day-form-container">
-
+            <form onSubmit={handleSubmit} className="add-day-form">
+                <label htmlFor='name'> Name
+                <input
+                    type="text"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    name="name"
+                    placeholder="Name"
+                    autoComplete="off"
+                />
+                </label>
+                {workoutInputList.map((input, index) => {
+                    return (
+                        <label htmlFor="workoutName" key={index}> {`Workout ${index + 1}`}
+                            <input
+                                type="text"
+                                name="workoutName"
+                                value={input}
+                                onChange={e => updateWorkoutInputList(e, index)}
+                                placeholder={`Workout #${index + 1}`}
+                            />
+                        </label>
+                    )
+                })}
+            </form>
         </div>
     )
 }
