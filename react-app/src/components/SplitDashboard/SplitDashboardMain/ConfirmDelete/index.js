1b0c3e4ddb87c5dcfea3203b1836d7fb35f4528f
Seth Corbett <scorbz9@gmail.com>
2022-03-29 14:18:09 -0700

    added popup confirm delete to delete day button

--- react-app/src/components/SplitDashboard/SplitDashboardMain/ConfirmDelete/index.js
+++ react-app/src/components/SplitDashboard/SplitDashboardMain/ConfirmDelete/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 
+import './ConfirmDelete.css'
+
 const ConfirmDelete = ({ typeOfDelete, handleDeleteDay, toggleDelete }) => {
 
 
 
     return (
 
-        <div className="confirm-delete-container">
-            <p className="confirm-delete-text">
-                Are you sure you'd like to delete this {typeOfDelete}?
-            </p>
-            <button className="confirm-delete-button" onClick={handleDeleteDay}>Delete {typeOfDelete[0].toUpperCase() + typeOfDelete.slice(1)}</button>
-            <button className="confirm-delete-cancel" onClick={toggleDelete}>Cancel</button>
+        <div className="overlay-wrapper">
+            <div className="confirm-delete-container">
+                <p className="confirm-delete-text">
+                    Are you sure you'd like to delete this {typeOfDelete}?
+                </p>
+                <div className="confirm-delete-button-container">
+                    <button className="confirm-delete-button" onClick={handleDeleteDay}>Delete {typeOfDelete[0].toUpperCase() + typeOfDelete.slice(1)}</button>
+                    <button className="confirm-delete-cancel" onClick={toggleDelete}>Cancel</button>
+                </div>
+            </div>
         </div>
     )
 }
